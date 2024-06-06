96db16b97d058a783d0c3b9d390043883b522e99
David Aparicio <david.aparicio@free.fr>
2024-06-06 09:13:21 +0200

    🐛 fix: redis internal/external

--- deploy.sh
+++ deploy.sh
@@ -2,5 +2,6 @@
 
 set -e
 
+# helm dependency build
 helm upgrade --install dockercoins .
-#helm upgrade --install dockercoins . --set redis.useSubChart=false
+# helm upgrade --install dockercoins . --set redis.useSubChart=false
