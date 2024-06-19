ef0305fc324995fcf5f305a05146cafad0f85cd9
David Aparicio <david.aparicio@free.fr>
2024-06-19 23:50:13 +0200

    ✨ feat: Debug+Getting the NOTES.txt data from a subchart #2751

--- deploy.sh
+++ deploy.sh
@@ -3,5 +3,10 @@
 set -e
 
 # helm dependency build
-helm upgrade --install dockercoins .
-# helm upgrade --install dockercoins . --set redis.useSubChart=false
+# helm upgrade --install dockercoins .
+helm upgrade --install dockercoins . --render-subchart-notes
+# helm status dockercoins
+# helm upgrade --install dockercoins . --render-subchart-notes #--set redis.useSubChart=false
+## https://github.com/helm/helm/issues/2751#issuecomment-1217426803
+# helm get notes dockercoins
+# helm template --debug dockercoins . --set redis.useInternal=true --set redis.useExternal=false #--set redis.service.port=3333
